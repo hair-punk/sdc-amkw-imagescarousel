53527a86385167efb8cc585de38f1b97ee3576c6
Alyssa Wei <wei.alyssa@gmail.com>
2019-06-04 14:55:41 -0700

    Setup: npm install and AWS config

--- database/aws.js
+++ database/aws.js
@@ -1,31 +1,31 @@
 const AWS = require('aws-sdk');
-const Credentials = require ('../config.js');
+const Credentials = require ('../awsconfig.json');
 const mongoose = require('mongoose');
 
 //configuring the AWS environment
-//This function will retrieve the Credentials from config.js.
+//This function will retrieve the Credentials from awsconfig.json.
 //These credentials have been created in Amazon S3. The name of the Bucket is the name of the Bucket created in S3 where I have previously stored 100 images related to products.
 
 const seeder = async function () {
   try {
     //connects to Amazon S3
     AWS.config.update({
-      accessKeyId: Credentials.AccessKeyID,
-      secretAccessKey: Credentials.SecretAccessKey,
-      region: 'us-east-1'
+      accessKeyId: Credentials.accessKeyID,
+      secretAccessKey: Credentials.secretAccessKey,
+      region: Credentials.region
     });
     //Access the Bucket and retrieves all files in the Bucket
     s3 = new AWS.S3();
     const response = await s3.listObjectsV2({
-      Bucket: 'renderingimagesforetsy'
-    }).promise()
+      Bucket: 'hrr38-sdcteam3-imagecarousel'
+    }).promise();
 
-    var contents = response.Contents
+    var contents = response.Contents;
     //make sure the table is empty before populating it with content from the Amazon S3 Bucket
-    deleteTable()
+    deleteTable();
     //for some reason a simple for loop wouldn't work here but a for of loop did.
     for (var image of contents){
-      let newImage = new Image ({url: 'https://s3.amazonaws.com/renderingimagesforetsy/' + image.Key})
+      let newImage = new Image({ url: 'https://s3.amazonaws.com/hrr38-sdcteam3-imagecarousel/' + image.Key})
       newImage.save()
 
     }
